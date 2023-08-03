468d0fe66d89e6b4dfbe32e8befc9931f02f04a3
boy-without-a-name <murenkov95@gmail.com>
2023-08-03 20:08:10 +0300

    refactor: add HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    entry: path.resolve(__dirname,'./src/index.ts'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|svg)$/i,
+                use: ['file-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'eCommerce'
+        }),
+        
+    ],
 };
