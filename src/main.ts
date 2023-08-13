426c7bc4b4cc439e6168a564374d279fc071755a
boy-without-a-name <murenkov95@gmail.com>
2023-08-13 16:43:11 +0300

    refactor: add husky

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,3 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
-
-// тест husky
-let test=function ():           void        {
-              console.log('тест хаски')      ;
-              console.log('test')
-};
-test();
