3599f65dff2dc12bb5d9fa175ddc1c827ba8a944
boy-without-a-name <murenkov95@gmail.com>
2023-08-13 15:51:15 +0300

    test huski

--- eCommerce-application/src/main.ts
+++ eCommerce-application/src/main.ts
@@ -5,3 +5,9 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  // тест husky
+let test=function(): void{
+                                                  console.log('тест хаски')
+}
+test();
