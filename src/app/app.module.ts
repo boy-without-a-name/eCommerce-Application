9bb400b3fa14badcb9471197b571fd99c8b2a219
boy-without-a-name <murenkov95@gmail.com>
2023-08-17 21:27:08 +0300

    fix: add AppLoginComponent in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppLogin } from './components/login/login.component';
+import { AppLoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MainComponent } from './components/main/main.component';
@@ -18,7 +18,7 @@ import { ButtonComponent } from './components/button/button.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AppLogin,
+    AppLoginComponent,
     MainComponent,
     HeaderComponent,
     CartComponent,
