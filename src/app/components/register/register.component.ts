41ba8046a98f7efb0398649d284953ba48733d9e
Vyacheslav Byrdaev <smit467bs@gmail.com>
2023-08-18 19:47:58 +0300

    feat: registr function

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,13 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {RegisterService} from '../../services/register.service';
-import {AccessTokenResponse, IRegisterData} from '../../services/types';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-
+import { Component, OnInit } from '@angular/core';
+import { RegisterService } from '../../services/register.service';
+import { AccessTokenResponse, IRegisterData } from '../../services/types';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
   error: boolean = false;
@@ -15,21 +14,19 @@ export class RegisterComponent implements OnInit {
     firstName: '',
     lastName: '',
     email: '',
-    password: ''
+    password: '',
   };
 
-  constructor(private http: HttpClient,
-              public service: RegisterService) {
-  }
+  constructor(
+    private http: HttpClient,
+    public service: RegisterService,
+  ) {}
 
   onSubmit(event: Event) {
     event.preventDefault();
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   // data = this.service.register();
-
-
 }
