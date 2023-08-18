10a0b4cd2ece79eeb5fa0895e6aed7d1e7125e3e
Vyacheslav Byrdaev <smit467bs@gmail.com>
2023-08-18 19:47:33 +0300

    feat: registr function

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,28 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { RegisterService } from '../../services/register.service';
-import { AccessTokenResponse, IRegisterData } from '../../services/types';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {RegisterService} from '../../services/register.service';
+import {AccessTokenResponse, IRegisterData} from '../../services/types';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss'],
+  styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
-  // error: boolean = false;
-  // data: IRegisterData = {
-  //   firstName: '',
-  //   lastName: '',
-  //   email: '',
-  //   password: '',
-  // };
-  // constructor(
-  //   private http: HttpClient,
-  //   public service: RegisterService,
-  // ) {}
-  // onSubmit(event: Event) {
-  //   event.preventDefault();
-  // }
-  // ngOnInit(): void {}
+export class RegisterComponent implements OnInit {
+  error: boolean = false;
+  data: IRegisterData = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  };
+
+  constructor(private http: HttpClient,
+              public service: RegisterService) {
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+  }
+
+  ngOnInit(): void {
+  }
+
   // data = this.service.register();
+
+
 }
