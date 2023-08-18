147336e140c901b6af44666549330d2b8f21c024
Daniil Shitikov <tochka098@gmail.com>
2023-08-18 13:32:46 +0400

    feat: define routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainViewComponent } from './components/main-view/main-view.component';
+import { AppLoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainViewComponent },
+  { path: 'login', component: AppLoginComponent },
+  { path: 'registration', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
