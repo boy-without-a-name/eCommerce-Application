11f57770cbe8b19e4002bfb4bfebf6c6050fe39f
Daniil Shitikov <tochka098@gmail.com>
2023-08-27 19:23:21 +0400

    feat: add profile page layout

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  user = {
+    firstName: localStorage.getItem('firstName'),
+    lastName: localStorage.getItem('lastName'),
+    email: localStorage.getItem('email'),
+  };
+}
