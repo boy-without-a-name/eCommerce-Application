3ba5c7e42502305f95a6377a59aa1a09b19b37a2
Daniil Shitikov <tochka098@gmail.com>
2023-08-27 20:45:48 +0400

    feat: add unauth guard (deny access to profile if not logged in)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,16 @@ import { MainViewComponent } from './components/main-view/main-view.component';
 import { AppLoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NotFoundViewComponent } from './components/not-found-view/not-found-view.component';
-import { authenticationGuard } from './shared/functions/auth.guard';
 import { ProfileComponent } from './components/profile/profile.component';
+import { authenticatedGuard } from './shared/functions/auth.guard';
+import { unauthenticatedGuard } from './shared/functions/unauth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' },
   { path: 'main', component: MainViewComponent },
-  { path: 'login', component: AppLoginComponent, canActivate: [authenticationGuard()] },
-  { path: 'registration', component: RegisterComponent, canActivate: [authenticationGuard()] },
-  { path: 'profile', component: ProfileComponent, canActivate: [authenticationGuard()] },
+  { path: 'login', component: AppLoginComponent, canActivate: [authenticatedGuard()] },
+  { path: 'registration', component: RegisterComponent, canActivate: [authenticatedGuard()] },
+  { path: 'profile', component: ProfileComponent, canActivate: [unauthenticatedGuard()] },
   { path: '**', component: NotFoundViewComponent },
 ];
 
