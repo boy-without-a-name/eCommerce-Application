44df4013ff75ab9a955fd4bcf5617ee34f3f7fb9
boy-without-a-name <murenkov95@gmail.com>
2023-08-28 16:16:45 +0300

    feat: add caatalog page

---
+++ src/app/components/catalog/catalog.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.scss'],
+})
+export class CatalogComponent {}
