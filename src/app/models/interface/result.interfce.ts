72887167664c735a75d3049f6c72e30ac54300bc
boy-without-a-name <murenkov95@gmail.com>
2023-08-28 17:57:54 +0300

    refactor: add catalog interface

---
+++ src/app/models/interface/result.interfce.ts
@@ -0,0 +1,122 @@
+export interface ResultInterface {
+  createdAt?: string;
+  createdBy?:{
+    isPlatformClient?: boolean;
+     user?: {
+      id?: string;
+      typeId?: string;
+     }
+  }
+  id?: string;
+  lastMessageSequenceNumber?: number;
+  lastModifiedAt: string;
+   lastModifiedBy?: {
+    isPlatformClient: boolean;
+    user?: {
+      id?: string;
+      typeId?: string;
+     }
+   }
+  lastVariantId: number
+  masterData?: {
+    current?: {
+      categories?: [];
+      categoryOrderHints?:{string: string};
+      description?: {
+        'en-US'?: string;
+      }
+      assets?: [];
+      attributes: [];
+      id?: number;
+      images?: [];
+      prices?: [
+        {
+          value?: {
+            type?: string;
+            fractionDigits?: number;
+            centAmount?: number;
+            currencyCode?: string;
+          },
+          id?: string;
+        }
+      ];
+      metaDescription?: {
+        'en-US'?: string;
+        'ru'?: string;
+      };
+      metaTitle?: {
+        'en-US'?: string;
+        'ru'?: string;
+      };
+      name?: {
+        'en-US'?: string;
+        'ru'?: string;
+      };
+      searchKeywords?: {
+        'en-US'?: string;
+        'ru'?: string;
+      };
+      slug?:  {
+        'en-US'?: string;
+        'ru'?: string;
+      };
+      variants?: [];
+    }
+    hasStagedChanges?: boolean;
+    published?: boolean;
+    staged?: {
+      categories?: [];
+      categoryOrderHints?:{string: string};
+      description?: {
+        'en-US'?: string;
+      }
+      assets?: [];
+      attributes: [];
+      id?: number;
+      images?: [
+        {
+          url: string;
+        }
+      ];
+      prices?: [
+        {
+          value?: {
+            type?: string;
+            fractionDigits?: number;
+            centAmount?: number;
+            currencyCode?: string;
+          },
+          id?: string;
+        }
+      ];
+      metaDescription?: {
+        'en-US'?: string;
+        'ru'?: string;
+      };
+      metaTitle?: {
+        'en-US'?: string;
+        'ru'?: string;
+      };
+      name?: {
+        'en-US'?: string;
+        'ru'?: string;
+      };
+      searchKeywords?: {
+        'en-US'?: string;
+        'ru'?: string;
+      };
+      slug?:  {
+        'en-US'?: string;
+        'ru'?: string;
+      };
+      variants?: [];
+    }
+  }
+  priceMode?: string;
+  productType?: {
+    id?: string;
+    typeId?: string;
+   }
+  version?: string;
+  versionModifiedAt?: string;
+}
