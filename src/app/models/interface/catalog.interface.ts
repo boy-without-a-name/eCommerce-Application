08f265171cd829525c7b52db4d8888185dbe5984
boy-without-a-name <murenkov95@gmail.com>
2023-08-28 19:07:52 +0300

    feat: add CatalogInterface

---
+++ src/app/models/interface/catalog.interface.ts
@@ -0,0 +1,10 @@
+import { ResultInterface } from "./result.interfce";
+
+export interface  CatalogInterface {
+  count?: number;
+  limit?: number;
+  offset?: number;
+  results?: ResultInterface[];
+  total?: number;
+}
+
