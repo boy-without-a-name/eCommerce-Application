3710e0a44d4d3047e6a4a35452b0498fc72e8f32
boy-without-a-name <murenkov95@gmail.com>
2023-08-28 16:42:26 +0300

    feat: add catalog.service

--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -7,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatalogComponent implements OnInit {
   ngOnInit(): void {
-    if(localStorage.getItem('userID') == (null || undefined)) {
-
+    if (localStorage.getItem('userID') == (null || undefined)) {
     }
   }
 }
---
+++ src/app/services/catalog/catalog.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { DataUser } from 'src/app/models/interface/dataUser.interface';
+import { AccessTokenResponse } from 'src/app/models/interface/AnswerTokenResponseInterface';
+import { scope, clientId, clientSecret, tokenUrlLogin } from 'src/app/models/constants/constants';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CatalogService {
+  answer: AccessTokenResponse = {
+    access_token: '',
+    expires_in: 0,
+    scope: '',
+    token_type: '',
+  };
+
+  constructor(private http: HttpClient) {}
+
+  getToken(): Observable<AccessTokenResponse> | null {
+    const data = new URLSearchParams();
+    data.append('grant_type', 'client_credentials');
+    data.append('scope', scope);
+
+    const headers = new HttpHeaders()
+      .set('Authorization', `Basic ${btoa(`${clientId}:${clientSecret}`)}`)
+      .set('Content-Type', 'application/x-www-form-urlencoded');
+
+    return this.http.post(tokenUrlLogin, data.toString(), { headers });
+  }
+
+  getProgucts(authToken: string | null): Observable<DataUser> | null {
+    const headers = new HttpHeaders()
+     .set('Authorization', `Bearer ${authToken}`)
+     .set('Content-Type', 'application/json');
+    return this.http.get('https://api.australia-southeast1.gcp.commercetools.com/arandomteam16/products', { headers });
+  }
+}
