94fe26c1a4f274e2d769095209624dd970ff8c4a
Vyacheslav Byrdaev <smit467bs@gmail.com>
2023-08-30 16:08:29 +0300

    refactor: delete unused

--- src/app/services/getProduct/get-product.service.ts
+++ src/app/services/getProduct/get-product.service.ts
@@ -7,24 +7,13 @@ import { Router } from '@angular/router';
   providedIn: 'root',
 })
 export class GetProductService {
-  idKey: string = '0';
   apiUrl = 'https://api.australia-southeast1.gcp.commercetools.com/arandomteam16/products/';
   id = '2168611f-2c95-423a-8692-b9ba4e46a719';
-  productResp: Current | undefined;
 
   constructor(
     private http: HttpClient,
     private router: Router,
   ) {}
-
-  setKey(id: string) {
-    this.idKey = `${id}`;
-  }
-
-  getProductData() {
-    this.renderProduct();
-    return this.productResp;
-  }
 
   getProduct(id: string = this.id) {
     const headers = new HttpHeaders({
