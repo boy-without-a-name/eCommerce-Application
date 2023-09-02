30786eff3df938e8230d676bda720bf310e2abe1
boy-without-a-name <murenkov95@gmail.com>
2023-09-02 15:10:24 +0300

    pretter

--- src/app/models/interface/results.filter.intreface.ts
+++ src/app/models/interface/results.filter.intreface.ts
@@ -10,39 +10,22 @@ export interface CardFilterInterface {
   };
   description: {
     'en-US': string;
-    categories: [];
-    categoryOrderHints: { strung?: string };
-    slug: {
-      'en-US': string;
-    };
-    metaTitle: {
-      'en-US': string;
-      ru: string;
-    };
-    metaDescription: {
-      'en-US': string;
-    };
-    variants: [
-      {
-        attributes: string[];
-        assets: [];
-        images: [
-          {
-            url: string;
-            dimensions: {
-              w: number;
-              h: number;
-            };
-          },
-        ];
-        prices: [];
-        key: string;
-        sku: string;
-        id: number;
-      },
-    ];
-    masterVariant: {
-      attributes: [];
+  };
+  categories: [];
+  categoryOrderHints: { strung?: string };
+  slug: {
+    'en-US': string;
+  };
+  metaTitle: {
+    'en-US': string;
+    ru: string;
+  };
+  metaDescription: {
+    'en-US': string;
+  };
+  variants: [
+    {
+      attributes: string[];
       assets: [];
       images: [
         {
@@ -52,35 +35,52 @@ export interface CardFilterInterface {
             h: number;
           };
         },
-        {
-          url: string;
-          dimensions: {
-            w: number;
-            h: number;
-          };
-        },
       ];
-      prices: [
-        {
-          id: string;
-          value: {
-            type: string;
-            currencyCode: string;
-            centAmount: number;
-            fractionDigits: number;
-          };
-        },
-      ];
+      prices: [];
       key: string;
       sku: string;
       id: number;
-    };
-    searchKeywords: { strung?: string };
-    hasStagedChanges: boolean;
-    published: boolean;
+    },
+  ];
+  masterVariant: {
+    attributes: [];
+    assets: [];
+    images: [
+      {
+        url: string;
+        dimensions: {
+          w: number;
+          h: number;
+        };
+      },
+      {
+        url: string;
+        dimensions: {
+          w: number;
+          h: number;
+        };
+      },
+    ];
+    prices: [
+      {
+        id: string;
+        value: {
+          type: string;
+          currencyCode: string;
+          centAmount: number;
+          fractionDigits: number;
+        };
+      },
+    ];
     key: string;
-    priceMode: string;
-    createdAt: string;
-    lastModifiedAt: string;
+    sku: string;
+    id: number;
   };
+  searchKeywords: { strung?: string };
+  hasStagedChanges: boolean;
+  published: boolean;
+  key: string;
+  priceMode: string;
+  createdAt: string;
+  lastModifiedAt: string;
 }
