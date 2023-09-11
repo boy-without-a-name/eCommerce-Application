a7a8b2d52eb8cacf084fe3bfc8c1848e47cbc323
boy-without-a-name <murenkov95@gmail.com>
2023-09-11 19:17:42 +0300

    add cart interface

---
+++ src/app/models/interface/carts.interface.ts
@@ -0,0 +1,46 @@
+export interface CartInterface {
+   type: string;
+    id: string;
+    version: number;
+    versionModifiedAt: string;
+    lastMessageSequenceNumber: number;
+    createdAt: string;
+    lastModifiedAt: string;
+    lastModifiedBy: {
+        clientId: string;
+        isPlatformClient: boolean;
+        customer: {
+            typeId: string;
+            id: string;
+          }
+    },
+    createdBy: {
+        clientId: string;
+        isPlatformClient: boolean;
+        customer: {
+            typeId: string;
+            id: string;
+        }
+    },
+    lineItems: [],
+    cartState: string;
+    totalPrice: {
+        type: string;
+        currencyCode: string;
+        centAmount: number;
+        fractionDigits: number;
+    },
+    shippingMode: string;
+    shipping: [],
+    customLineItems: [],
+    discountCodes: [],
+    directDiscounts: [],
+    inventoryMode: string;
+    taxMode: string;
+    taxRoundingMode: string;
+    taxCalculationMode: string;
+    deleteDaysAfterLastModification: number;
+    refusedGifts: [],
+    origin: string;
+    itemShippingAddresses: []
+}
