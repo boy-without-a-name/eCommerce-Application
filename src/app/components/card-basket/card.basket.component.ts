5392a594bd6f4c35804cd3bbc34b36418f049ce8
boy-without-a-name <murenkov95@gmail.com>
2023-09-14 12:34:06 +0300

    feat: add html for card Basket

--- src/app/components/card-basket/card.basket.component.ts
+++ src/app/components/card-basket/card.basket.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProductCart } from 'src/app/models/interface/cartProduct.interface';
 
 @Component({
   selector: 'app-card-basket',
   templateUrl: './card.basket.component.html',
   styleUrls: ['./card.basket.component.scss'],
 })
-export class CardBasketComponent {}
+export class CardBasketComponent {
+  @Input() product: ProductCart;
+}
