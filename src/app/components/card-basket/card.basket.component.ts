c083c496b87c5e3f7f40749d86a50f2efa798b1d
boy-without-a-name <murenkov95@gmail.com>
2023-09-14 16:36:07 +0300

    clickPlus to increase the quantity of goods by one

--- src/app/components/card-basket/card.basket.component.ts
+++ src/app/components/card-basket/card.basket.component.ts
@@ -38,4 +38,25 @@ export class CardBasketComponent {
         });
     }
   }
+  clickPlus(valueInput: string, lineItemId: string): void {
+    localStorage.setItem('idCart', '97fde447-d06d-4168-afd7-72f50fd196cf');
+    this.carts
+      .changeLineItemQuantity(
+        localStorage.getItem('token'),
+        lineItemId,
+        Number(localStorage.getItem('version')),
+        Number(valueInput),
+        localStorage.getItem('idCart'),
+        1,
+      )
+      ?.subscribe((res) => {
+        localStorage.version = res.version;
+        console.log(res.version);
+        this.showTotalPrice = false;
+        this.curentQuantity = res.lineItems.filter((item) => item.id === lineItemId)[0].quantity.toString();
+        this.totalPrice = (
+          res.lineItems.filter((item) => item.id === lineItemId)[0].totalPrice.centAmount / 100
+        ).toString();
+      });
+  }
 }
