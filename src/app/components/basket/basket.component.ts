27c53927d4136627bcc60c9fac2c870fcbf7cf89
boy-without-a-name <murenkov95@gmail.com>
2023-09-14 11:31:25 +0300

    feat: add a method ngOnInit to get the cart

--- src/app/components/basket/basket.component.ts
+++ src/app/components/basket/basket.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/carts/carts.service';
+import { ProductCart } from 'src/app/models/interface/cartProduct.interface';
 @Component({
   selector: 'app-basket',
   templateUrl: './basket.component.html',
   styleUrls: ['./basket.component.scss'],
 })
-export class BasketComponent {}
+export class BasketComponent implements OnInit {
+  showLinkCatalog = false;
+  product: ProductCart[] = [];
+
+  constructor(private basket: CartService) {}
+
+  ngOnInit(): void {
+    this.basket.getCart('97fde447-d06d-4168-afd7-72f50fd196cf', localStorage.getItem('token'))?.subscribe({
+      next: (response) => {
+        if (response.lineItems.length === 0) {
+          this.showLinkCatalog = true;
+        } else {
+          this.product = response.lineItems;
+        }
+      },
+    });
+  }
+}
