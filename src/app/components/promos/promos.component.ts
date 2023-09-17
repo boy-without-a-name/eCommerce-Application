b57de63f9a80f44badf0300c2a74ace01d8d2018
Daniil Shitikov <tochka098@gmail.com>
2023-09-18 00:24:58 +0400

    feat: implement copy-on-click functionality to promo codes

--- src/app/components/promos/promos.component.ts
+++ src/app/components/promos/promos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-promos',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./promos.component.scss'],
 })
 export class PromosComponent {
+  constructor(private clipboard: Clipboard) {}
+
   promos = [{ name: '10% off each cart item (for carts > 3000 USD)', code: 'massive-discount' }];
+
+  copyPromoCode(promoCodeToCopy: string): void {
+    this.clipboard.copy(promoCodeToCopy);
+  }
 }
