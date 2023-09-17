aa5d38fcba9a3c0e2edbb52cbe692e44aae5bbb9
boy-without-a-name <murenkov95@gmail.com>
2023-09-17 14:27:21 +0300

    refactor:create class Cardevent for common methods

--- src/app/components/card-product/card.component.ts
+++ src/app/components/card-product/card.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { ResultInterface } from 'src/app/models/interface/result.interfce';
-import { GetProductService } from '../../services/getProduct/get-product.service';
-import { CartService } from 'src/app/services/carts/carts.service';
+import { GetProductService } from 'src/app/services/getProduct/get-product.service';
+import { CardEvent } from 'src/app/shared/class/cardEvent';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -9,65 +9,18 @@ import { CartService } from 'src/app/services/carts/carts.service';
 })
 export class CardComponent {
   buttonPosition = false;
-  btnDisabled = false;
   constructor(
+    private cardEvent: CardEvent,
     public getProductService: GetProductService,
-    private carts: CartService,
   ) {}
   @Input() product: ResultInterface;
 
-  saveLocalStorage(productId: string): void {
-    if (localStorage.getItem('cartsProductId') == null) {
-      localStorage.setItem('cartsProductId', '{}');
-    }
-    const data = JSON.parse(localStorage.getItem('cartsProductId') as string);
-    if (data[`${productId}`] == undefined) {
-      data[`${productId}`] = productId;
-    }
-    localStorage.cartsProductId = JSON.stringify(data);
-  }
-
   clickBtn(productId: string): void {
-    if (localStorage.getItem('idCart') == null) {
-      this.carts.createCart(localStorage.getItem('token'))?.subscribe((res) => {
-        localStorage.setItem('idCart', `${res.id}`);
-        localStorage.setItem('version', `${res.version}`);
-        this.carts
-          .addLineItem(
-            localStorage.getItem('token'),
-            productId,
-            Number(localStorage.getItem('version')),
-            localStorage.getItem('idCart'),
-          )
-          ?.subscribe((res) => {
-            localStorage.version = res.version;
-            this.saveLocalStorage(productId);
-          });
-      });
-    } else {
-      this.carts
-        .addLineItem(
-          localStorage.getItem('token'),
-          productId,
-          Number(localStorage.getItem('version')),
-          localStorage.getItem('idCart'),
-        )
-        ?.subscribe((res) => {
-          localStorage.version = res.version;
-          this.saveLocalStorage(productId);
-        });
-    }
+    this.cardEvent.clickBtn(productId);
     this.buttonPosition = true;
-    this.btnDisabled = true;
   }
 
   disabled(productId: string): boolean {
-    if (localStorage.getItem('cartsProductId')) {
-      const data = JSON.parse(localStorage.getItem('cartsProductId') as string);
-      if (data[productId]) {
-        return true;
-      }
-    }
-    return false;
+    return this.cardEvent.disabled(productId);
   }
 }
---
+++ src/app/shared/class/cardEvent.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { CartService } from 'src/app/services/carts/carts.service';
+import { GetProductService } from 'src/app/services/getProduct/get-product.service';
+@Injectable({
+  providedIn: 'root',
+})
+export class CardEvent {
+  constructor(
+    public getProductService: GetProductService,
+    private carts: CartService,
+  ) {}
+
+  saveLocalStorage(productId: string): void {
+    if (localStorage.getItem('cartsProductId') == null) {
+      localStorage.setItem('cartsProductId', '{}');
+    }
+    const data = JSON.parse(localStorage.getItem('cartsProductId') as string);
+    if (data[`${productId}`] == undefined) {
+      data[`${productId}`] = productId;
+    }
+    localStorage.cartsProductId = JSON.stringify(data);
+  }
+
+  addLineItem(productId: string): void {
+    this.carts
+      .addLineItem(
+        localStorage.getItem('token'),
+        productId,
+        Number(localStorage.getItem('version')),
+        localStorage.getItem('idCart'),
+      )
+      ?.subscribe((res) => {
+        localStorage.version = res.version;
+        this.saveLocalStorage(productId);
+      });
+  }
+
+  clickBtn(productId: string): void {
+    if (localStorage.getItem('idCart') == null) {
+      this.carts.createCart(localStorage.getItem('token'))?.subscribe((res) => {
+        localStorage.setItem('idCart', `${res.id}`);
+        localStorage.setItem('version', `${res.version}`);
+        this.addLineItem(productId);
+      });
+    } else {
+      this.addLineItem(productId);
+    }
+  }
+
+  disabled(productId: string): boolean {
+    if (localStorage.getItem('cartsProductId')) {
+      const data = JSON.parse(localStorage.getItem('cartsProductId') as string);
+      if (data[productId]) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
