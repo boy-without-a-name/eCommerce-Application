940477a748888a99ddfa666db30711d16af222e9
boy-without-a-name <murenkov95@gmail.com>
2023-09-18 20:37:24 +0300

    fix: click btn method merge

--- src/app/components/card-product/card.component.ts
+++ src/app/components/card-product/card.component.ts
@@ -1,10 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { ResultInterface } from 'src/app/models/interface/result.interfce';
-
-import { GetProductService } from '../../services/getProduct/get-product.service';
-import { CartService } from 'src/app/services/carts/carts.service';
+import { GetProductService } from 'src/app/services/getProduct/get-product.service';
 import { CardEvent } from 'src/app/shared/class/cardEvent';
-
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -19,43 +16,8 @@ export class CardComponent {
   @Input() product: ResultInterface;
 
   clickBtn(productId: string): void {
-    if (localStorage.getItem('idCart') == null) {
-      this.carts.createCart(localStorage.getItem('token'))?.subscribe((res) => {
-        localStorage.setItem('idCart', `${res.id}`);
-        localStorage.setItem('version', `${res.version}`);
-        this.carts
-          .addLineItem(
-            localStorage.getItem('token'),
-            productId,
-            Number(localStorage.getItem('version')),
-            localStorage.getItem('idCart'),
-          )
-          ?.subscribe((res) => {
-            localStorage.version = res.version;
-            this.carts.updateTotalQuantity(res.totalLineItemQuantity);
-          });
-      });
-    } else {
-      this.carts
-        .addLineItem(
-          localStorage.getItem('token'),
-          productId,
-          Number(localStorage.getItem('version')),
-          localStorage.getItem('idCart'),
-        )
-        ?.subscribe({
-          next: (res) => {
-            localStorage.version = res.version;
-            this.carts.updateTotalQuantity(res.totalLineItemQuantity);
-          },
-          error: (err) => {
-            console.log(err.error.message);
-          },
-        });
-    }
-
     this.cardEvent.clickBtn(productId);
-
+    this.buttonPosition = true;
   }
 
   disabled(productId: string): boolean {
