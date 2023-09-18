eb878f11d71f16fab1cd4bba1f15aec65d9f4344
boy-without-a-name <murenkov95@gmail.com>
2023-09-18 19:35:19 +0300

    pretter

--- src/app/shared/class/reRecordCart.ts
+++ src/app/shared/class/reRecordCart.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@angular/core';
 import { CartService } from 'src/app/services/carts/carts.service';
 
-
 @Injectable({
   providedIn: 'root',
 })
 export class ReRecordCart {
   constructor(private cart: CartService) {}
-
 
   reRecordCart(): void {
     if (localStorage.getItem('idCart')) {
@@ -15,14 +13,11 @@ export class ReRecordCart {
         localStorage.getItem('cartsProductId') &&
         Object.keys(JSON.parse(localStorage.getItem('cartsProductId') as string)).length > 0
       ) {
-        this.cart.replicateCart(localStorage.getItem('token'), localStorage.getItem('idCart'))?.subscribe((res)=>{
+        this.cart.replicateCart(localStorage.getItem('token'), localStorage.getItem('idCart'))?.subscribe((res) => {
           localStorage.idCart = res.id;
           localStorage.version = res.version;
-        })
-
+        });
       }
     }
   }
-
-  }
-
+}
